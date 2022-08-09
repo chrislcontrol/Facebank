0dfc3eae19f692f444cf4c82335d91cfc388af6d
chrislcontrol <christian.pereira@nexxera.com>
2022-08-09 01:16:44 -0300

    feat: exception handler

--- src/main/adapter/flask_adapter.py
+++ src/main/adapter/flask_adapter.py
@@ -1,11 +1,18 @@
 from src.main.interface.route import IRoute
+from src.presenters.utils.exception_handler import ExceptionHandler
 from src.presenters.utils.http_request import HttpRequest
 
 
 class FlaskAdapter:
+    exception_handler = ExceptionHandler()
+
     def adapt(self, request: any, api_route: IRoute):
-        http_request = HttpRequest(
-            headers=request.headers, body=request.json, params=request.args.to_dict()
-        )
-        response = api_route.route(http_request)
-        return response
+        try:
+            http_request = HttpRequest(
+                headers=request.headers, body=request.json, params=request.args.to_dict()
+            )
+            response = api_route.route(http_request)
+            return response
+
+        except Exception as exc:
+            return self.exception_handler.handle(exc)
